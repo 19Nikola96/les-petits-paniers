bdb158d2eca38ab7dfa6b33ca787cdeb206f0180
19Nikola96 <niko9mg@gmail.com>
2020-08-11 14:35:03 +0200

    modif info perso et pop in

--- src/Controller/PaymentPathController.php
+++ src/Controller/PaymentPathController.php
@@ -8,11 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class PaymentPathController extends AbstractController
 {
     /**
-     * @Route("/payment/path", name="payment_path")
+     * @Route("/payment/path")
      */
     public function index()
     {
         return $this->render('payment_path/index.html.twig', [
+            'controller_name' => 'PaymentPathController',
+        ]);
+    }
+
+    /**
+     * @Route("/payment/path2")
+     */
+    public function index2()
+    {
+        return $this->render('payment_path/index2.html.twig', [
             'controller_name' => 'PaymentPathController',
         ]);
     }
